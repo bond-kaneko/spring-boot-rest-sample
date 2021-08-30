e82532481d3fbc87f7f78f7c4cc98295aa2a6fd4
junki.kaneko <junki.kaneko@linecorp.com>
2021-08-30 18:09:13 +0900

    intial

---
+++ src/main/java/com/example/restplayground/restplayground/RestPlayGroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.restplayground.restplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPlayGroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestPlayGroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restplayground/restplayground/RestPlayGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restplayground.restplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestPlayGroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
